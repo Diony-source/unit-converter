3aada06fa550293cce3d83d8e8774e3a18bd7f47
Diony-source <erendalkiran2001@gmail.com>
2024-11-19 13:58:09 +0300

    Implement logic for km_to_miles, kg_to_pounds, and c_to_f conversions

--- services/services.go
+++ services/services.go
@@ -2,16 +2,17 @@ package services
 
 import (
 	"errors"
+	"unit-converter/entities"
 )
 
-func Convert(c Conversion) (float64, error) {
+func Convert(c entities.Conversion) (float64, error) {
 	switch c.Type {
 	case "km-to-miles":
 		return c.Value * 0.621371, nil
 	case "kg-to-pounds":
 		return c.Value * 2.20462, nil
 	case "c-to-f":
-		return (c.Value*9/5) + 32, nil
+		return (c.Value * 9/5) + 32, nil
 	default:
 		return 0, errors.New("unsupported conversion type")
 	}
